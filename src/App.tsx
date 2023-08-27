3ed8bb9012a992cd8a77ef081cbf9497e2f20d10
kopytyoshka <ilyatttt19@gmail.com>
2023-08-27 14:01:47 +0300

    feat: add active products tab

--- src/App.tsx
+++ src/App.tsx
@@ -9,12 +9,14 @@ import {
     IonRouterOutlet,
     setupIonicReact
 } from '@ionic/react';
-import {cardOutline, personOutline, bookOutline} from 'ionicons/icons';
+import {cardOutline, personOutline, bookOutline, cashOutline} from 'ionicons/icons';
 import {Redirect, Route} from "react-router-dom";
 import LearningPage from "./pages/LearningPage";
 import ProfilePage from "./pages/ProfilePage";
 import CreditOffersPage from "./pages/CreditOffersPages";
 import {IonReactRouter} from "@ionic/react-router";
+import ActiveCreditsPage from "./pages/ActiveCreditsPage";
+import './styles/App.css';
 
 setupIonicReact();
 const App: React.FC = () => (
@@ -25,20 +27,25 @@ const App: React.FC = () => (
                     <Route path="/credit-offers" component={CreditOffersPage} exact/>
                     <Route path="/profile" component={ProfilePage} exact/>
                     <Route path="/learning" component={LearningPage} exact/>
+                    <Route path="/active-offers" component={ActiveCreditsPage} exact/>
                     <Redirect from="/" to="/credit-offers" exact/>
                 </IonRouterOutlet>
-                <IonTabBar slot="bottom">
+                <IonTabBar slot="bottom" className="custom-tab-bar">
                     <IonTabButton tab="search" href="/credit-offers">
                         <IonIcon icon={cardOutline}/>
-                        <IonLabel>Поиск продуктов</IonLabel>
+                        <IonLabel>Поиск</IonLabel>
                     </IonTabButton>
                     <IonTabButton tab="profile" href="/profile">
                         <IonIcon icon={personOutline}/>
-                        <IonLabel>Личный кабинет</IonLabel>
+                        <IonLabel>Аккаунт</IonLabel>
                     </IonTabButton>
                     <IonTabButton tab="learning" href="/learning">
                         <IonIcon icon={bookOutline}/>
                         <IonLabel>Обучение</IonLabel>
+                    </IonTabButton>
+                    <IonTabButton tab="bank" href="/active-offers">
+                        <IonIcon icon={cashOutline}/>
+                        <IonLabel>Кредиты</IonLabel>
                     </IonTabButton>
                 </IonTabBar>
             </IonTabs>
